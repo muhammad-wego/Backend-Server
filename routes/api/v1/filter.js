7aa8935a5612d00681caaa7ddd36d380eb130cac
akashdeepb <akashdeepb.cs17@sahyadri.edu.in>
2020-11-29 09:38:37 -0800

    Added Filter

--- routes/api/v1/filter.js
+++ routes/api/v1/filter.js
@@ -5,8 +5,16 @@ const ObjectId = require('mongodb').ObjectId;
 const AuthController = require('../../../contollers/AuthController');
 
 router.post('/:type/:healthParam/:query',AuthController.verify_token,AuthController.is_authorized,function(req,res){
-    record.find({parameters:{$elemMatch:{healthParameter:ObjectId(req.params.healthParam)}}}).then(records => {return res.status(200).json({records});})
-    .catch(err=>{return res.status(500).json({message : "Internal Server Error"});})
+    record.find({parameters:{$elemMatch:{healthParameter:ObjectId(req.params.healthParam)}}}).then(records => {
+        let response = [];
+        records.forEach((currentRecord,i) => {
+            if(currentRecord.parameters[req.params.type] == req.params.query)
+                response.push(currentRecord);
+        });
+        return res.status(200).json({records:response});
+    }).catch(err => {
+        return res.status(500).json({message : "Internal Server Error"});
+    })
 });
 
 module.exports = router;
