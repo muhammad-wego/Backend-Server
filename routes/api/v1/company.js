9852102cfc772e4d5a8970b2935f5c73b5d00b61
USER <hmbilal.998@gmail.com>
2021-01-24 12:14:45 +0530

    Changed monthlyOverview added avgWeight

--- routes/api/v1/company.js
+++ routes/api/v1/company.js
@@ -716,6 +716,8 @@ async function (req, res){
       medium: 0,
       good: 0,
     };
+    let weightSum = 0
+    let pcount = 0
     const HealthParameters = await healthParameter.find();
     let HealthParamStages = new Array();
     for (const Parameter of HealthParameters) {
@@ -741,6 +743,9 @@ async function (req, res){
           personnelScoresObj.medium += 1;
         else personnelScoresObj.good += 1;
 
+        weightSum+=ph.weight
+        pcount+=1
+
         for(const hp of ph.parameters){
           for(const param of HealthParamStages){
             if(String(hp.healthParameter) == String(param.ParameterId)){
@@ -757,8 +762,8 @@ async function (req, res){
       }
        
     }
-      
-    res.status(200).json({HealthParamStages,personnelScoresObj});
+    let avgWeight = parseFloat((weightSum/pcount).toFixed(2)); 
+    res.status(200).json({HealthParamStages,personnelScoresObj,avgWeight});
 
     }else{
       return res.staus(401).json({message:"Unauthorized"});
